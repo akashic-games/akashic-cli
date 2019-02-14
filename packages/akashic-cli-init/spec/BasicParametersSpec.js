60516ca6fe4a727a7f582203537649853bc6dd8f
dera- <alchemy.and.fairy@gmail.com>
2019-02-14 20:25:42 +0900

    delete mock-fs from BasicParameterSpec in akashic-cli-init

--- packages/akashic-cli-init/spec/BasicParametersSpec.js
+++ packages/akashic-cli-init/spec/BasicParametersSpec.js
@@ -1,6 +1,5 @@
 var mockPrompt = require("./support/mockPrompt");
 var bp = require("../lib/BasicParameters");
-var mockfs = require("mock-fs");
 var fs = require("fs-extra");
 var os = require("os");
 var path = require("path");
@@ -12,25 +11,23 @@ describe("BasicParameters", function () {
 		var quietLogger = new commons.ConsoleLogger({quiet: true});
 
 		beforeEach(() => {
-			mockfs({});
 			mockPrompt.mock({ width: 42, height: 27, fps: 30 });
 		});
 
 		afterEach(() => {
 			mockPrompt.restore();
-			mockfs.restore();
 		});
 
-		// it("update game.json", done => {
-		// 	var conf = { width: 12, height: 23, fps: 34, assets: {} };
-		// 	fs.writeJsonSync(confPath, conf);
-		// 	bp.updateConfigurationFile(confPath, quietLogger)
-		// 		.then(() => {
-		// 			expect(fs.readJsonSync(confPath))
-		// 				.toEqual({width: 42, height: 27, fps: 30, assets: {}});
-		// 		})
-		// 		.then(done, done.fail);
-		// });
+		it("update game.json", done => {
+			var conf = { width: 12, height: 23, fps: 34, assets: {} };
+			fs.writeJsonSync(confPath, conf);
+			bp.updateConfigurationFile(confPath, quietLogger)
+				.then(() => {
+					expect(fs.readJsonSync(confPath))
+						.toEqual({width: 42, height: 27, fps: 30, assets: {}});
+				})
+				.then(done, done.fail);
+		});
 
 		describe("parameter value is not number", () => {
 			it("value is NaN", done => {
