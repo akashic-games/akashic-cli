8347dcaf246e97030febb73580799c342392c03f
dera- <alchemy.and.fairy@gmail.com>
2019-03-05 22:02:53 +0900

    fix generateFilesAboutEngineFiles.js and update engineFiles to v2.1.11

--- packages/akashic-cli-export-html/build-template-support/generateFilesAboutEngineFiles.js
+++ packages/akashic-cli-export-html/build-template-support/generateFilesAboutEngineFiles.js
@@ -4,6 +4,8 @@ var fetch = require("node-fetch");
 var execSync = require("child_process").execSync;
 
 console.log("start to generate files");
+var jsonPath = path.join(__dirname, "..", "templates", "engineFilesVersion.json");
+var currentVersions = require(jsonPath);
 var v1Version = execSync(`npm info @akashic/engine-files@for_ae1x version`).toString().replace("\n", "");
 var v2Version = execSync(`npm info @akashic/engine-files@latest version`).toString().replace("\n", "");
 
@@ -12,17 +14,20 @@ var versions = {
 	v1: v1Version,
 	v2: v2Version
 };
-fs.writeFileSync(path.join(__dirname, "..", "templates", "engineFilesVersion.json"), JSON.stringify(versions, null, 2));
+fs.writeFileSync(jsonPath, JSON.stringify(versions, null, 2));
 console.log("end to generate engineFilesVersion.json");
 
-var promises = Object.keys(versions).map(majorVersion => {
-	var fileName = `engineFilesV${versions[majorVersion].replace(/\./g, "_")}.js`;
+// v1,v2のうち、バージョンが更新されたもののjsファイルのみをダウンロード
+var updatedVersions = Object.keys(versions).filter(v => versions[v] !== currentVersions[v]).map(v => versions[v]);
+var promises = updatedVersions.map(version  => {
+	var fileName = `engineFilesV${version.replace(/\./g, "_")}.js`;
 	return Promise.resolve().then(() => {
-		console.log(`start to download engineFiles (v${versions[majorVersion]})`);
-		return fetch(`https://github.com/akashic-games/engine-files/releases/download/v${versions[majorVersion]}/${fileName}`);
+		console.log(`start to download engineFiles (v${version})`);
+		return fetch(`https://github.com/akashic-games/engine-files/releases/download/v${version}/${fileName}`);
 	}).then(res => {
 		return new Promise((resolve, reject) => {
-			var fileName = `engineFilesV${versions[majorVersion].replace(/\./g, "_")}.js`;
+			var fileName = `engineFilesV${version.replace(/\./g, "_")}.js`;
+			var majorVersion = `v${version.substring(0, 1)}`;
 			var fileStream = fs.createWriteStream(
 				path.join(__dirname, "..", "templates", `template-export-html-${majorVersion}`, "js", fileName)
 			);
@@ -34,7 +39,7 @@ var promises = Object.keys(versions).map(majorVersion => {
 				resolve();
 			});
 		});
-	}).then(() => {console.log(`end to download engineFiles (v${versions[majorVersion]})`);});
+	}).then(() => {console.log(`end to download engineFiles (v${version})`);});
 });
 Promise.all(promises)
 	.then(() => {console.log("end to generate files");})
