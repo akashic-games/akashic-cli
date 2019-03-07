8a89dddbe3a42114e556b73cfb25da882fcbcd15
dera- <alchemy.and.fairy@gmail.com>
2019-03-08 04:01:03 +0900

    fix detail

--- packages/akashic-cli-serve/src/server/controller/ConfigController.ts
+++ packages/akashic-cli-serve/src/server/controller/ConfigController.ts
@@ -3,7 +3,7 @@ import * as EngineConfig from "../domain/EngineConfig";
 import { serverGlobalConfig } from "../common/ServerGlobalConfig";
 
 export const createHandlerToGetEngineConfig = (baseDir: string, isRaw: boolean): express.RequestHandler => {
-	return async (req, res, next) => {
+	return (req, res, next) => {
 		try {
 			const urlInfo = req.header("host").split(":");
 			// ポート番号が見つからなかった場合、httpのデフォルト番号の80とする
@@ -13,7 +13,7 @@ export const createHandlerToGetEngineConfig = (baseDir: string, isRaw: boolean):
 			const hostname = serverGlobalConfig.useGivenHostname ? serverGlobalConfig.hostname : urlInfo[0];
 			const port = serverGlobalConfig.useGivenPort ? serverGlobalConfig.port : parseInt(urlInfo[1], 10);
 			const baseUrl = `http://${hostname}:${port}`;
-			const engineConfigJson = await EngineConfig.getEngineConfig(baseUrl, baseDir, isRaw);
+			const engineConfigJson = EngineConfig.getEngineConfig(baseUrl, baseDir, isRaw);
 			// akashic-gameview側でレスポンスがengineConfigJsonの形式なっていることを前提にしているので、resoponseSuccessは使わない
 			res.status(200).json(engineConfigJson);
 		} catch (e) {
