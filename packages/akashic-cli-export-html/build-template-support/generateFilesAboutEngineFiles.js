08835f58085b4ddce71fac9db8ef9cb1b3445329
dera- <alchemy.and.fairy@gmail.com>
2019-03-12 22:01:16 +0900

    fix generateFilesAboutEngineFiles.js and update engineFiles

--- packages/akashic-cli-export-html/build-template-support/generateFilesAboutEngineFiles.js
+++ packages/akashic-cli-export-html/build-template-support/generateFilesAboutEngineFiles.js
@@ -20,8 +20,6 @@ if (fs.existsSync(jsonPath)) {
 }
 var v1Version = execSync(`npm info @akashic/engine-files@for_ae1x version`).toString().replace("\n", "");
 var v2Version = execSync(`npm info @akashic/engine-files@latest version`).toString().replace("\n", "");
-
-console.log("start to generate engineFilesVersion.json");
 var versions = {
 	v1: {
 		version: v1Version,
@@ -32,8 +30,6 @@ var versions = {
 		variable: `engineFilesV${v2Version.replace(/\./g, "_")}`
 	}
 };
-fs.writeFileSync(jsonPath, JSON.stringify(versions, null, 2));
-console.log("end to generate engineFilesVersion.json");
 
 // v1,v2のうち、バージョンが更新されたもののjsファイルのみをダウンロード
 var promises = Object.keys(versions).filter(v => versions[v].version !== currentVersions[v].version).map(v  => {
@@ -41,6 +37,9 @@ var promises = Object.keys(versions).filter(v => versions[v].version !== current
 	var fileName = `${versions[v].variable}.js`;
 	console.log(`start to download engineFiles (v${version})`);
 	return fetch(`https://github.com/akashic-games/engine-files/releases/download/v${version}/${fileName}`).then(res => {
+		if (res.status >= 400) {
+			throw new Error(`failed to get engine-files. status:${res.status}.`);
+		}
 		return new Promise((resolve, reject) => {
 			var majorVersion = `v${version.substring(0, 1)}`;
 			var fileStream = fs.createWriteStream(
@@ -57,6 +56,10 @@ var promises = Object.keys(versions).filter(v => versions[v].version !== current
 	}).then(() => console.log(`end to download engineFiles (v${version})`));
 });
 Promise.all(promises)
+	.then(() => {
+		console.log("start to generate engineFilesVersion.json");
+		fs.writeFileSync(jsonPath, JSON.stringify(versions, null, 2));
+		console.log("end to generate engineFilesVersion.json");})
 	.then(() => console.log("end to generate files"))
 	.catch(err => {
 		console.error(err);
