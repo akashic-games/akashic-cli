fa86125a4eef521984d880d138fc8f0902e31151
kamakiri01 <kamakiri01@users.noreply.github.com>
2019-03-12 17:53:27 +0900

    check game.json update

--- packages/akashic-cli-serve/src/server/controller/ScriptAssetController.ts
+++ packages/akashic-cli-serve/src/server/controller/ScriptAssetController.ts
@@ -1,19 +1,25 @@
 import * as fs from "fs";
 import * as path from "path";
 import * as express from "express";
+import * as chokidar from "chokidar";
 
 export const createScriptAssetController = (baseDir: string): express.RequestHandler => {
+	const gameJsonPath = path.join(baseDir, "game.json");
+	let gameJson: any = JSON.parse(fs.readFileSync(gameJsonPath).toString());
+
+	const watcher = chokidar.watch(gameJsonPath, { persistent: true });
+	watcher.on("change", () => {
+		console.log("onchanged");
+	    gameJson = JSON.parse(fs.readFileSync(gameJsonPath).toString());
+	});
 	return (req: express.Request, res: express.Response, next: Function): void => {
 		const scriptPath = path.join(baseDir, req.params.scriptName);
-		const gameJsonPath = path.join(baseDir, "game.json");
 		if (!fs.existsSync(scriptPath) || !fs.existsSync(scriptPath)) {
 			const err: any = new Error("Not Found");
 			err.status = 404;
 			next(err);
 			return;
 		}
-		// TODO: chokidar等でgame.jsonの変更時だけ読み込みを行うようにする
-		const gameJson: any = JSON.parse(fs.readFileSync(gameJsonPath).toString());
 		let id = Object.keys(gameJson.assets).find((id) => gameJson.assets[id].path === req.params.scriptName);
 		const content = fs.readFileSync(scriptPath);
 		const responseBody = `
