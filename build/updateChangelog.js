7c78d80dd826c4db9c4829f702a5a53e031fa57c
dera- <alchemy.and.fairy@gmail.com>
2019-03-27 15:30:28 +0900

    fix updateChangelog script

--- build/updateChangelog.js
+++ build/updateChangelog.js
@@ -1,3 +1,4 @@
+// ※このスクリプトを実行するためには環境変数GITHUB_AUTHにgithubへのアクセストークンを与える必要があります
 const path = require("path");
 const fs = require("fs");
 const semver = require("semver");
@@ -8,16 +9,19 @@ if (process.argv.length < 3) {
 	process.exit(1);
 }
 
-// 対象のバージョンナンバー名を取得
+// どのバージョンを上げるのかを取得
 var target = process.argv[2];
 if (! /^patch|minor|major$/.test(target)) {
 	console.error("please specify patch or minor or major");
 	process.exit(1);
 }
 
-const modules = JSON.parse(execSync(`${path.join(__dirname, "..", "node_modules", ".bin", "lerna")} ls --toposort --json`).toString().replace("\n", ""));
-const currentVersion = modules[modules.length - 1]["version"];
+// 全akashic-cli-xxxに依存するakashic-cliモジュールの次のバージョン番号を取得
+const packageJson = require(path.join(__dirname, "..", "packages", "akashic-cli", "package.json"));
+const currentVersion = packageJson["version"];
 const nextVersion = semver.inc(currentVersion, target);
+
+// 現在のCHANGELOGに次バージョンのログを追加
 const currentChangeLog = fs.readFileSync(path.join(__dirname, "..", "CHANGELOG.md")).toString();
 const addedLog = execSync(`${path.join(__dirname, "..", "node_modules", ".bin", "lerna-changelog")}`).toString().replace("Unreleased", nextVersion);
 const nextChangeLog = currentChangeLog.replace("# CHANGELOG\n\n", "# CHANGELOG\n" + addedLog + "\n");
