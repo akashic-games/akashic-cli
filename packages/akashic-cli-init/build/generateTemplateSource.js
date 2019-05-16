138fd002f7c8a971639d40235c58aeec93452658
dera- <alchemy.and.fairy@gmail.com>
2019-05-16 20:30:12 +0900

    fix generateTemplateSource.js and npm-script

--- packages/akashic-cli-init/build/generateTemplateSource.js
+++ packages/akashic-cli-init/build/generateTemplateSource.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const shell = require('shelljs');
 const { execSync } = require("child_process");
 
 const templatesSrcDirPath = path.join(__dirname, "..", "templates-src");
@@ -25,10 +26,10 @@ const templateData = {
 console.log("Start to generate typescript-templates");
 Object.keys(templateData).forEach(key => {
 	console.log(`"${key}" template`);
-	execSync(`rm -rf ${path.join(templatesDirPath, templateData[key]["ts-dist"])}`);
-	execSync(`cp -r ${path.join(templatesSrcDirPath, templateData[key]["src"])} ${path.join(templatesDirPath, templateData[key]["ts-dist"])}`);
-	execSync(`cp -r ${path.join(templatesSrcDirPath, "typescript-base", "*")} ${path.join(templatesDirPath, templateData[key]["ts-dist"])}`);
-	execSync(`cp ${path.join(templatesSrcDirPath, "typescript-base", ".gitignore")} ${path.join(templatesDirPath, templateData[key]["ts-dist"])}`);
+	shell.rm("-rf", path.join(templatesDirPath, templateData[key]["ts-dist"]));
+	shell.cp("-R", path.join(templatesSrcDirPath, templateData[key]["src"]), path.join(templatesDirPath, templateData[key]["ts-dist"]));
+	shell.cp("-R", path.join(templatesSrcDirPath, "typescript-base", "*"), path.join(templatesDirPath, templateData[key]["ts-dist"]));
+	shell.cp(path.join(templatesSrcDirPath, "typescript-base", ".gitignore"), path.join(templatesDirPath, templateData[key]["ts-dist"]));
 });
 console.log("End to generate typescript-templates");
 
@@ -36,19 +37,25 @@ console.log("End to generate typescript-templates");
 console.log("Start to generate javascript-templates");
 Object.keys(templateData).forEach(key => {
 	console.log(`"${key}" template`);
-	execSync(`rm -rf ${path.join(templatesDirPath, templateData[key]["js-dist"])}`);
-	execSync(`cp -r ${path.join(templatesSrcDirPath, templateData[key]["src"])} ${path.join(templatesDirPath, templateData[key]["js-dist"])}`);
-	execSync(`cp ${path.join(templatesSrcDirPath, "typescript-base", "package.json")} ${path.join(templatesDirPath, templateData[key]["js-dist"])}`);
-	execSync(`cp ${path.join(templatesSrcDirPath, "typescript-base", "tsconfig.json")} ${path.join(templatesDirPath, templateData[key]["js-dist"])}`);
+	shell.rm("-rf", path.join(templatesDirPath, templateData[key]["js-dist"]));
+	shell.cp("-R", path.join(templatesSrcDirPath, templateData[key]["src"]), path.join(templatesDirPath, templateData[key]["js-dist"]));
+	shell.cp(path.join(templatesSrcDirPath, "typescript-base", "package.json"), path.join(templatesDirPath, templateData[key]["js-dist"]));
+	shell.cp(path.join(templatesSrcDirPath, "typescript-base", "tsconfig.json"), path.join(templatesDirPath, templateData[key]["js-dist"]));
 	console.log("  - start to install packages and build");
+	// インストールとビルドが完了するのを待ちたいので、ここだけ execSync を使用する
 	execSync(`cd ${path.join(templatesDirPath, templateData[key]["js-dist"])} && npm install && npm run build`);
 	console.log("  - end to install packages and build");
-	execSync(`rm -rf ${path.join(templatesDirPath, templateData[key]["js-dist"], "src")}`);
-	execSync(`rm -rf ${path.join(templatesDirPath, templateData[key]["js-dist"], "node_modules")}`);
-	execSync(`rm ${path.join(templatesDirPath, templateData[key]["js-dist"], "package.json")}`);
-	execSync(`rm ${path.join(templatesDirPath, templateData[key]["js-dist"], "package-lock.json")}`);
-	execSync(`rm ${path.join(templatesDirPath, templateData[key]["js-dist"], "tsconfig.json")}`);
-	execSync(`cp -r ${path.join(templatesSrcDirPath, "javascript-base", "*")} ${path.join(templatesDirPath, templateData[key]["js-dist"])}`);
-	execSync(`cp ${path.join(templatesSrcDirPath, "javascript-base", ".eslintrc.json")} ${path.join(templatesDirPath, templateData[key]["js-dist"])}`);
+	shell.rm(
+		"-rf",
+		path.join(templatesDirPath, templateData[key]["js-dist"], "src"),
+		path.join(templatesDirPath, templateData[key]["js-dist"], "node_modules")
+	);
+	shell.rm(
+		path.join(templatesDirPath, templateData[key]["js-dist"], "package.json"),
+		path.join(templatesDirPath, templateData[key]["js-dist"], "package-lock.json"),
+		path.join(templatesDirPath, templateData[key]["js-dist"], "tsconfig.json")
+	);
+	shell.cp("-R", path.join(templatesSrcDirPath, "javascript-base", "*"), path.join(templatesDirPath, templateData[key]["js-dist"]));
+	shell.cp(path.join(templatesSrcDirPath, "javascript-base", ".eslintrc.json"), path.join(templatesDirPath, templateData[key]["js-dist"]));
 });
 console.log("End to generate javascript-templates");
