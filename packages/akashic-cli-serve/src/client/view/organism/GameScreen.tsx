f87942e66534ba2cea33031fe4cc77568d4f86f4
xnv <xnv@users.noreply.github.com>
2019-10-01 20:28:57 +0900

    add default background image

--- packages/akashic-cli-serve/src/client/view/organism/GameScreen.tsx
+++ packages/akashic-cli-serve/src/client/view/organism/GameScreen.tsx
@@ -15,22 +15,21 @@ export interface GameScreenProps {
 @observer
 export class GameScreen extends React.Component<GameScreenProps, {}> {
 	render(): React.ReactNode {
+		const { showsBackgroundImage: showsBgImage, backgroundImage: bgImage, showsGrid, gameWidth, gameHeight } = this.props;
+		const bgImageStyle = (showsBgImage && !bgImage) ?  (" " + styles["pseudo-transparent-bg"]) : "";
 		return <div className={styles["game-screen"]}>
 			{
-				this.props.showsBackgroundImage ?
-					<img src={this.props.backgroundImage} className={styles["bg-image"]}/> :
+				(showsBgImage && bgImage) ?
+					<img src={bgImage} className={styles["bg-image"]}/> :
 					null
 			}
-			<div className={styles["game-content"]} ref={this._onRef} />
+			<div className={styles["game-content"] + bgImageStyle} ref={this._onRef} />
 			{
-				this.props.showsGrid ?
+				showsGrid ?
 					<canvas
 						id="gridCanvas"
 						className={styles["grid-canvas"]}
-						style={{
-							width: this.props.gameWidth + "px",
-							height: this.props.gameHeight + "px"
-						}}
+						style={{ width: gameWidth, height: gameHeight }}
 						ref={this._createGridCanvas}/> :
 					null
 			}
@@ -44,19 +43,20 @@ export class GameScreen extends React.Component<GameScreenProps, {}> {
 	}
 
 	private _createGridCanvas = (gridCanvas: HTMLCanvasElement): void => {
-		if (gridCanvas) {
-			gridCanvas.width = this.props.gameWidth;
-			gridCanvas.height = this.props.gameHeight;
-			const context = gridCanvas.getContext("2d");
-			context.save();
-			context.strokeStyle = "#CCC";
-			context.setLineDash([3, 3]);
-			this._drawGridLine(context, 20, 20);
-			context.strokeStyle = "#AAA";
-			context.setLineDash([]);
-			this._drawGridLine(context, 100, 100);
-			context.restore();
-		}
+		if (!gridCanvas)
+			return;
+
+		gridCanvas.width = this.props.gameWidth;
+		gridCanvas.height = this.props.gameHeight;
+		const context = gridCanvas.getContext("2d");
+		context.save();
+		context.strokeStyle = "#CCC";
+		context.setLineDash([3, 3]);
+		this._drawGridLine(context, 20, 20);
+		context.strokeStyle = "#AAA";
+		context.setLineDash([]);
+		this._drawGridLine(context, 100, 100);
+		context.restore();
 	}
 
 	private _drawGridLine = (context: CanvasRenderingContext2D, gridWidth: number, gridHeight: number): void => {
