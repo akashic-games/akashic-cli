5399cbcfcd55d3b6c9bb519ba2a09e973bb9add4
dera- <alchemy.and.fairy@gmail.com>
2019-10-17 13:43:42 +0900

    reflect comment

--- packages/akashic-cli-serve/src/server/route/HealthCheckRoute.ts
+++ packages/akashic-cli-serve/src/server/route/HealthCheckRoute.ts
@@ -7,9 +7,9 @@ export interface HealthCheckRouterParameterObject {
 }
 
 export const createHealthCheckRouter = (params: HealthCheckRouterParameterObject): express.Router => {
-	const contentsRouter = express.Router();
+	const healthCheckRouter = express.Router();
 
-	contentsRouter.get(`/status`, createHandleToGetStatus(params.playStore));
+	healthCheckRouter.get(`/status`, createHandleToGetStatus(params.playStore));
 
-	return contentsRouter;
+	return healthCheckRouter;
 };
