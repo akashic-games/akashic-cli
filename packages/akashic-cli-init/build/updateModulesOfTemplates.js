c631ee017f3d90f185964766a8d1e23933ab13e7
dera- <alchemy.and.fairy@gmail.com>
2020-01-09 10:04:42 +0000

    fix to use npm-check-updates

--- packages/akashic-cli-init/build/updateModulesOfTemplates.js
+++ packages/akashic-cli-init/build/updateModulesOfTemplates.js
@@ -1,20 +1,15 @@
 const fs = require("fs");
 const path = require("path");
-const shell = require('shelljs');
+const shell = require("shelljs");
 
 const templateSrcPath = path.join(__dirname, "..", "templates-src");
 const templates = fs.readdirSync(templateSrcPath).filter(dir => fs.existsSync(path.join(templateSrcPath, dir, "package.json")));
 
 console.log("Start to update modules of templates");
+const ncuPath = path.join(__dirname, "..", "node_modules", ".bin", "ncu");
 templates.forEach(name => {
 	console.log(`- ${name}`);
-	const templatePath = path.join(templateSrcPath, name);
-	const packageJson = require(path.join(templatePath, "package.json"));
-	const targetModules = Object.keys(packageJson["devDependencies"]).filter(m => /^@akashic\//.test(m));
-	shell.cd(templatePath);
-	targetModules.forEach(module => {
-		console.log(`  - ${module}`);
-		shell.exec(`npm install --save-dev ${module}@latest`);
-	});
+	shell.cd(path.join(templateSrcPath, name));
+	shell.exec(`${ncuPath} -u "/^@akashic\/.+$/" --loglevel verbose --packageFile package.json`);
 });
 console.log("End to update modules of templates");
