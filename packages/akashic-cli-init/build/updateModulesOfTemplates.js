23491f08f02f03dd6da219e184e94c835a6bb2ec
dera- <alchemy.and.fairy@gmail.com>
2020-01-10 07:09:07 +0000

    reflect comment(use npm-check-updates api)

--- packages/akashic-cli-init/build/updateModulesOfTemplates.js
+++ packages/akashic-cli-init/build/updateModulesOfTemplates.js
@@ -1,15 +1,16 @@
 const fs = require("fs");
 const path = require("path");
-const shell = require("shelljs");
+const ncu = require("npm-check-updates");
 
 const templateSrcPath = path.join(__dirname, "..", "templates-src");
 const templates = fs.readdirSync(templateSrcPath).filter(dir => fs.existsSync(path.join(templateSrcPath, dir, "package.json")));
 
 console.log("Start to update modules of templates");
-const ncuPath = path.join(__dirname, "..", "node_modules", ".bin", "ncu");
-templates.forEach(name => {
-	console.log(`- ${name}`);
-	shell.cd(path.join(templateSrcPath, name));
-	shell.exec(`${ncuPath} -u "/^@akashic\/.+$/" --loglevel verbose --packageFile package.json`);
+Promise.all(
+	templates.map((name) => {
+		return ncu.run({upgrade :true, packageFile: path.join(templateSrcPath, name, "package.json"), filter: "/^@akashic\/.+$/"})
+			.then(upgraded => console.log(`- ${name}:`, upgraded));
+	})
+).then(() => {
+	console.log("End to update modules of templates");
 });
-console.log("End to update modules of templates");
