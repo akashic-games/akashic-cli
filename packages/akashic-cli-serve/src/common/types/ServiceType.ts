f8efad635e8b68a76fe2344c3efba650ac88f62c
dera- <alchemy.and.fairy@gmail.com>
2020-01-30 11:23:56 +0000

    reflect comment

--- packages/akashic-cli-serve/src/common/types/ServiceType.ts
+++ packages/akashic-cli-serve/src/common/types/ServiceType.ts
@@ -1,7 +1,7 @@
 /**
  * --target-serviceの引数として使用される
- * nicolive: ニコ生として起動する
- * atsumaru: RPGアツマールとして起動する
+ * nicolive: ニコ生互換の環境として起動する
+ * atsumaru: RPGアツマール互換の環境として起動する
  * none: 通常起動(--target-serviceオプションなしの状態)
  */
 
