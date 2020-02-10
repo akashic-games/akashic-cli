e9f5c202951a42220adb5c5d42fd55445440268a
dera- <alchemy.and.fairy@gmail.com>
2020-02-10 03:55:33 +0000

    reflect comment

--- packages/akashic-cli-serve/src/client/view/molecule/AtsumaruDevtool.tsx
+++ packages/akashic-cli-serve/src/client/view/molecule/AtsumaruDevtool.tsx
@@ -15,20 +15,20 @@ export class AtsumaruDevtool extends React.Component<AtsumaruDevtoolProps, {}> {
 	render(): React.ReactNode {
 		const props = this.props;
 		return <div>
-		{
-			// TODO: ここのデザイン部分をユーザーフレンドリーなものに修正する
-			props.disabled && <p><b>このタブは現在無効になっています。有効にするためにはTargetServiceにatsumaruを指定する必要があります。</b></p>
-		}
-		volume:
-		<ToolProgressBar
-			width={200}
-			max={100} value={props.volume}
-			active={props.isSeekingVolume}
-			onChange={!props.disabled && props.changeVolume}
-			onCommit={!props.disabled && props.dicideVolume} />
-		{
-			`${props.volume} / 100`
-		}
-	</div>;
+			{
+				// TODO: ここのデザイン部分をユーザーフレンドリーなものに修正する
+				props.disabled && <p><b>このタブは現在無効になっています。有効にするためにはTargetServiceにatsumaruを指定する必要があります。</b></p>
+			}
+			volume:
+			<ToolProgressBar
+				width={200}
+				max={100} value={props.volume}
+				active={props.isSeekingVolume}
+				onChange={!props.disabled && props.changeVolume}
+				onCommit={!props.disabled && props.dicideVolume} />
+			{
+				`${props.volume} / 100`
+			}
+		</div>;
 	}
 }
