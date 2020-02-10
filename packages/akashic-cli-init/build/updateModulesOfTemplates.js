2f4d35e7886523e4cc4a2805c963a2ea45f231bc
dera- <alchemy.and.fairy@gmail.com>
2020-02-10 13:17:56 +0000

    add templates-src-v3 in akashic-cli-init

--- packages/akashic-cli-init/build/updateModulesOfTemplates.js
+++ packages/akashic-cli-init/build/updateModulesOfTemplates.js
@@ -3,14 +3,20 @@ const path = require("path");
 const ncu = require("npm-check-updates");
 
 const templateSrcPath = path.join(__dirname, "..", "templates-src");
-const templates = fs.readdirSync(templateSrcPath).filter(dir => fs.existsSync(path.join(templateSrcPath, dir, "package.json")));
+const templateSrcV3Path = path.join(__dirname, "..", "templates-src-v3");
 
-console.log("Start to update modules of templates");
-Promise.all(
-	templates.map((name) => {
-		return ncu.run({upgrade :true, packageFile: path.join(templateSrcPath, name, "package.json"), filter: "/^@akashic\/.+$/"})
-			.then(upgraded => console.log(`- ${name}:`, upgraded));
-	})
-).then(() => {
-	console.log("End to update modules of templates");
-});
+updateModules(templateSrcPath);
+updateModules(templateSrcV3Path);
+
+function updateModules(srcPath) {
+	const templates = fs.readdirSync(srcPath).filter(dir => fs.existsSync(path.join(srcPath, dir, "package.json")));
+	console.log(`Start to update modules of templates in ${srcPath}`);
+	Promise.all(
+		templates.map((name) => {
+			return ncu.run({upgrade :true, packageFile: path.join(srcPath, name, "package.json"), filter: "/^@akashic\/.+$/"})
+				.then(upgraded => console.log(`- ${name}:`, upgraded));
+		})
+	).then(() => {
+		console.log(`End to update modules of templates in ${srcPath}`);
+	});
+}
