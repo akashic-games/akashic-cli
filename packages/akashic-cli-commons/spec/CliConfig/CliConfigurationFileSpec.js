37e1ae77592c9b0d9e2d8e8cb866cb9ebcfa30c1
kamakiri01 <kamakiri01@users.noreply.github.com>
2020-03-18 20:12:39 +0900

    fix test

--- packages/akashic-cli-commons/spec/CliConfig/CliConfigurationFileSpec.js
+++ packages/akashic-cli-commons/spec/CliConfig/CliConfigurationFileSpec.js
@@ -1,36 +1,31 @@
 var mockfs = require("mock-fs");
+var path = require("path");
 var CliConfigurationFile = require("../../lib/CliConfig/CliConfigurationFile").CliConfigurationFile;
-var ConsoleLogger = require("../../lib/ConsoleLogger").ConsoleLogger;
 
 describe("ConfigurationFile", function () {
 
-	var mockCAkashicConfigJs = {
-		"akashic.config.js": `
-		var config = {
-			commandOptions: {
-				serve: {
-					port: 3030
+	describe(".read()", function () {
+
+		var mockCAkashicConfigJs = {
+			"akashic.config.js": `
+			var config = {
+				commandOptions: {
+					serve: {
+						port: 3030
+					}
 				}
 			}
-		}
-		
-		module.exports = config;
-		`
-	};
+			module.exports = config;
+			`
+		};
 
-	var logger, loggedResult;
-	beforeEach(function () {
-		loggedResult = [];
-		logger = new ConsoleLogger({ debugLogMethod: loggedResult.push.bind(loggedResult) });
-		mockfs(mockCAkashicConfigJs);
-	});
-	afterEach(function () {
-		mockfs.restore();
-	});
-
-	describe(".read()", function () {
+		beforeEach(function () {
+			mockfs(mockCAkashicConfigJs);
+		});
+		afterEach(function () {
+			mockfs.restore();
+		});
 		it("reads akashic.config.js", function (done) {
-			var path = require("path");
 			CliConfigurationFile.read(path.join(process.cwd(), "./akashic.config.js"), (error, config) => {
 				if (error) return done.fail();
 				expect(config).toEqual({
@@ -45,9 +40,48 @@ describe("ConfigurationFile", function () {
 		});
 		it("invalid path", function (done) {
 			CliConfigurationFile.read("./invalid/dir/akashic.config.js", (error, config) => {
-				if (error) return done();
 				expect(config).toEqual({
 					commandOptions: {}
+				});
+				done();
+			});
+		});
+	});
+});
+
+describe("invalid ConfigurationFile", function () {
+
+	var mockBrokenCAkashicConfigJs = {
+		"akashic.config.js": `
+		var config = {
+			commandOptions: {
+				serve: {
+					port: 30303030
+				}
+			}
+		}
+		module.exports = config;
+		`
+	};
+
+	describe(".read()", function () {
+		beforeEach(function () {
+			mockfs({
+
+			});
+		});
+		afterEach(function () {
+			mockfs.restore();
+		});
+		it("invalid akashic.config.js", function (done) {
+			CliConfigurationFile.read(path.join(process.cwd(), "./akashic.config.js"), (error, config) => {
+				if (error) return done();
+				expect(config).toEqual({
+					commandOptions: {
+						serve: {
+							port: 3000000000
+						}
+					}
 				});
 				done.fail();
 			});
