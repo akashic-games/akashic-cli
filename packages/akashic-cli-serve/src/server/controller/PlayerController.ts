b8dc04a0e9ffa6cdb992c291131ef4be6b296d85
dera- <alchemy.and.fairy@gmail.com>
2020-09-10 14:26:23 +0000

    fix player api

--- packages/akashic-cli-serve/src/server/controller/PlayerController.ts
+++ packages/akashic-cli-serve/src/server/controller/PlayerController.ts
@@ -9,11 +9,14 @@ export const createHandlerToRegisterPlayer = (playerStore: PlayerStore): express
 	return (req, res, next) => {
 		try {
 			const playerId = req.body.playerId;
+			const playerName = req.body.playerName;
 			let player = playerStore.getPlayer(playerId);
 			if (player) {
+				// 同一IDのプレイヤーが存在する且つ異なるプレイヤー名が指定された場合、サーバーに保存はしないがその名前を使用する
+				player = { id: player.id, name: playerName || player.name };
 				responseSuccess<PlayerPostApiResponseData>(res, 200, { player, isDuplicationId: true });
 			} else {
-				player = playerStore.registerPlayer({ id: playerId, name: req.body.playerName });
+				player = playerStore.registerPlayer({ id: playerId, name: playerName });
 				responseSuccess<PlayerPostApiResponseData>(res, 200, { player, isDuplicationId: false });
 			}
 		} catch (e) {
