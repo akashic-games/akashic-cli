0c13e926571de2ba3a953d96e599cec28bf6c022
kamakiri01 <kamakiri01@users.noreply.github.com>
2021-05-13 16:52:17 +0900

    revert restore

--- packages/akashic-cli-serve/src/client/akashic/ServeGameContent.ts
+++ packages/akashic-cli-serve/src/client/akashic/ServeGameContent.ts
@@ -76,6 +76,7 @@ export class ServeGameContent {
 			const mat = getMatrixFromRoot(e, camera || game.focusingCamera)._matrix;
 			renderer.transform(mat);
 			renderer.fillRect(0, 0, e.width, e.height, "rgba(255, 0, 0, 0.3)");
+			renderer.restore();
 			const anchorEdge = 4;
 			renderer.fillRect(
 				e.width * e.anchorX - anchorEdge / 2,
