11cb3a63ff727c11cf352d2ee2d26d3b15e71625
yu-ogi <yusuke_ogiwara@dwango.co.jp>
2021-07-01 09:53:51 +0900

    fix(scan): revert 1e251712

--- packages/akashic-cli-scan/src/AssetModule.ts
+++ packages/akashic-cli-scan/src/AssetModule.ts
@@ -144,7 +144,10 @@ export namespace AssetModule {
 				} else {
 					id = path.basename(asset.path);
 				}
-				if (!includeExtensionToAssetId) {
+				if (
+					asset.type !== "audio" && // NOTE: オーディオアセットは拡張子が含まれていないため無視
+					!includeExtensionToAssetId
+				) {
 					id = id.replace(/\.[^/.]+$/, ""); // 拡張子を削除
 				}
 			}
