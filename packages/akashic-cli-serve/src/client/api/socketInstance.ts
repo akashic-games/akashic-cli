19b8ebb29e48ccfd90264b593c3f4c97cab853c0
yu-ogi <yusuke_ogiwara@dwango.co.jp>
2021-07-15 11:00:03 +0900

    feat(serve): add msgpack parser

--- packages/akashic-cli-serve/src/client/api/socketInstance.ts
+++ packages/akashic-cli-serve/src/client/api/socketInstance.ts
@@ -1,11 +1,17 @@
+import type * as ioc from "socket.io-client";
+import parser from "../../common/MsgpackParser";
+
+declare var io: typeof ioc.io;
+
 const socket = io("ws://" + window.location.host, {
-	// デフォルトはInifinityだが、過去に同じホスト・ポートで起動されたサーバに
+	// デフォルトはInfinityだが、過去に同じホスト・ポートで起動されたサーバに
 	// 接続していたクライアントが生きている(i.e. ブラウザのタブが開いたまま)時、
 	// つなぎなおして来てしまうのでやむなく1にしている。(0だとInfinity扱いされる)
 	// 本当はここを 1 にするよりも、playId を起動ごとにユニークなものにすべきかもしれない。
-	reconnectionAttempts: 1
+	reconnectionAttempts: 1,
+	parser
 });
 
-export function socketInstance(): SocketIOClient.Socket {
+export function socketInstance(): ioc.Socket {
 	return socket;
 }
