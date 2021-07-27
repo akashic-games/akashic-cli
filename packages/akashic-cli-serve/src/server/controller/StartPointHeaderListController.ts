bb06989c027a1499d443c91b2873da0e1add4fe3
kamakiri01 <kamakiri01@users.noreply.github.com>
2021-07-27 18:15:33 +0900

    fix illegal playId flow

--- packages/akashic-cli-serve/src/server/controller/StartPointHeaderListController.ts
+++ packages/akashic-cli-serve/src/server/controller/StartPointHeaderListController.ts
@@ -1,6 +1,6 @@
 import * as express from "express";
 import { responseError, responseSuccess } from "../common/ApiResponse";
-import { NotFoundError } from "../common/ApiError";
+import { BadRequestError, NotFoundError } from "../common/ApiError";
 import { StartPointHeaderListResponseData } from "../../common/types/ApiResponse";
 import { StartPointHeader } from "../../common/types/StartPointHeader";
 import { PlayStore } from "../domain/PlayStore";
@@ -10,15 +10,18 @@ export const createHandlerToGetStartPointHeaderList = (playStore: PlayStore): ex
 	return (req, res, next) => {
 		try {
 			let playId = req.params.playId;
-			const amflow = playStore.createAMFlow(playId);
-			if (playId && amflow) {
-				const startPointHeaderList: StartPointHeader[] = amflow.dump().startPoints.map(startPoint => {
-					return { frame: startPoint.frame, timestamp: startPoint.timestamp };
-				});
-				responseSuccess<StartPointHeaderListResponseData>(res, 200, { startPointHeaderList });
-			} else {
-				responseError(res, new NotFoundError({ errorMessage: "startPoint not found" }));
+			if (!req.params.playId) {
+				throw new BadRequestError({ errorMessage: "PlayId is not given" });
 			}
+
+			const amflow = playStore.createAMFlow(playId);
+			if (!amflow) {
+				throw new NotFoundError({ errorMessage: `StartPoint is not found. playId:${playId}` });
+			}
+			const startPointHeaderList: StartPointHeader[] = amflow.dump().startPoints.map(startPoint => {
+				return { frame: startPoint.frame, timestamp: startPoint.timestamp };
+			});
+			responseSuccess<StartPointHeaderListResponseData>(res, 200, { startPointHeaderList });
 		} catch (e) {
 			next(e);
 		}
