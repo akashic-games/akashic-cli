69cb5d74e9e41267157fd6d71950fd4a0f8c06e4
xnv <xnv@users.noreply.github.com>
2021-09-16 18:41:06 +0900

    feat(serve): menhance progress bars: optional width, more draggable region

--- packages/akashic-cli-serve/src/client/view/stories/ToolProgressBar.stories.tsx
+++ packages/akashic-cli-serve/src/client/view/stories/ToolProgressBar.stories.tsx
@@ -1,7 +1,28 @@
 import * as React from "react";
+import { observable } from "mobx";
+import { observer } from "mobx-react";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 import { ToolProgressBar } from "../atom/ToolProgressBar";
+
+const store = observable({
+	max: 100,
+	value: 10,
+});
+
+const TestWithBehaviour = observer(() => (
+	<>
+		<div style={{ width: 300, padding: "0 14px", border: "1px solid red", resize: "horizontal", overflow: "auto" }}>
+			<ToolProgressBar
+				max={store.max}
+				value={store.value}
+				onChange={v => store.value = v}
+				onCommit={v => store.value = v}
+			/>
+		</div>
+		{ store.value } / { store.max }
+	</>
+));
 
 storiesOf("a-ToolProgressBar", module)
 	.add("basic", () => (
@@ -16,4 +37,8 @@ storiesOf("a-ToolProgressBar", module)
 	))
 	.add("max", () => (
 		<ToolProgressBar width={200} max={100} value={100} />
-	));
+	))
+	.add("no-width", () => (
+		<ToolProgressBar max={100} value={50} />
+	))
+	.add("with-behaviour", () => <TestWithBehaviour />);
