0981404e394b39769dcd30ecf68dcb43eda01abb
xnv <xnv@users.noreply.github.com>
2021-11-04 22:23:39 +0900

    fix(test): fix broken coverages

--- packages/akashic-cli-serve/jest.config.js
+++ packages/akashic-cli-serve/jest.config.js
@@ -20,7 +20,7 @@ module.exports = {
 	collectCoverage: true,
 
 	// An array of glob patterns indicating a set of files for which coverage information should be collected
-	collectCoverageFrom: ["src/**/*.{js}", "!**/node_modules/**"],
+	collectCoverageFrom: ["lib/**/*.js", "!**/node_modules/**"],
 
 	// The directory where Jest should output its coverage files
 	coverageDirectory: "coverage",
