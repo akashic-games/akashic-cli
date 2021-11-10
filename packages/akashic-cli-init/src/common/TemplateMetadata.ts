c8b3e7433ec10beb16b628363dcad36b0b5fccf9
xnv <xnv@users.noreply.github.com>
2021-11-10 11:09:26 +0900

    chore(init): (WIP) restore request for debug

--- packages/akashic-cli-init/src/common/TemplateMetadata.ts
+++ packages/akashic-cli-init/src/common/TemplateMetadata.ts
@@ -1,6 +1,7 @@
 import * as fs from "fs";
 import * as os from "os";
 import fetch from "node-fetch";
+import * as request from "request";
 import * as path from "path";
 import * as unzipper from "unzipper";
 import { readdir } from "@akashic/akashic-cli-commons/lib/FileSystem";
@@ -11,53 +12,53 @@ interface TemplateList {
 }
 
 export type LocalTemplateMetadata = {
-  sourceType: "local";
-  name: string;
-  path: string;
+	sourceType: "local";
+	name: string;
+	path: string;
 }
 
 export type RemoteTemplateMetadata = {
-  sourceType: "remote";
-  name: string;
-  url: string;
+	sourceType: "remote";
+	name: string;
+	url: string;
 }
 
 export type TemplateMetadata =
-  LocalTemplateMetadata |
-  RemoteTemplateMetadata;
+	LocalTemplateMetadata |
+	RemoteTemplateMetadata;
 
 export function digestOfTemplateMetadata(metadata: TemplateMetadata): string {
-  switch (metadata.sourceType) {
-    case "local": return metadata.path;
-    case "remote": return metadata.url;
-  }
+	switch (metadata.sourceType) {
+		case "local": return metadata.path;
+		case "remote": return metadata.url;
+	}
 }
 
 export async function collectLocalTemplatesMetadata(templateDirectory: string): Promise<LocalTemplateMetadata[]> {
 	try {
 		const dirnames = await readdir(templateDirectory);
-    return dirnames.map(dirname => ({
-      sourceType: "local",
-      name: dirname,
-      path: path.join(templateDirectory, dirname)
-    }));
+		return dirnames.map(dirname => ({
+			sourceType: "local",
+			name: dirname,
+			path: path.join(templateDirectory, dirname)
+		}));
 	} catch (err) {
 		if (err.code !== "ENOENT") throw err;
-    return [];
+		return [];
 	}
 }
 
 export async function fetchRemoteTemplatesMetadata(templateListJsonUri: string): Promise<RemoteTemplateMetadata[]> {
-  const res = await fetch(templateListJsonUri);
-  const templateListJson = (await res.json()) as TemplateList;
-  return Object.keys(templateListJson.templates).map(name => {
-    const rawUrl = templateListJson.templates[name];
-    return {
-      sourceType: "remote",
-      name,
-      url: new URL(rawUrl, templateListJsonUri).toString()
-    };
-  });
+	const res = await fetch(templateListJsonUri);
+	const templateListJson = (await res.json()) as TemplateList;
+	return Object.keys(templateListJson.templates).map(name => {
+		const rawUrl = templateListJson.templates[name];
+		return {
+			sourceType: "remote",
+			name,
+			url: new URL(rawUrl, templateListJsonUri).toString()
+		};
+	});
 }
 
 /**
@@ -69,18 +70,58 @@ export async function fetchRemoteTemplatesMetadata(templateListJsonUri: string):
  * @returns 取得したテンプレート(template.jsonがあるディレクトリ)のパス。テンポラリディレクトリでありうる。
  */
 export async function fetchTemplate(metadata: TemplateMetadata): Promise<string> {
-  switch (metadata.sourceType) {
-    case "local": {
-      return metadata.path;
-    }
-    case "remote": {
-      const { name, url } = metadata;
-      const zip = await (await fetch(url)).buffer();
-      const destDir = fs.mkdtempSync(path.join(os.tmpdir(), name));
-      await _extractZip(zip, path.join(destDir, name));
-      return destDir;
-    }
-  }
+	switch (metadata.sourceType) {
+		case "local": {
+			return metadata.path;
+		}
+		case "remote": {
+			const { name, url } = metadata;
+			// const zip = await (await fetch(url)).buffer();
+			const zip = await _request({ uri: url, method: "GET", encoding: null });
+			const destDir = fs.mkdtempSync(path.join(os.tmpdir(), name));
+			await _extractZip(zip, destDir);
+			return destDir;
+		}
+	}
+}
+
+
+interface RequestParameterObject {
+	/**
+	 * サーバURI
+	 */
+	uri: string;
+
+	/**
+	 * HTTPメソッド (例: "GET")
+	 */
+	method: string;
+
+	/**
+	 * 結果をjsonで返すかどうか
+	 */
+	json?: boolean;
+
+	/**
+	 * 結果のエンコーディング (nullを指定するとBuffer)
+	 */
+	encoding?: string;
+}
+
+function _request(param: RequestParameterObject): Promise<any> {
+	return new Promise<any>((resolve, reject) => {
+		request(param, (err: any, res: any, body: any) => {
+			if (err) {
+				reject(err);
+				return;
+			}
+			if (res.statusCode !== 200) {
+				reject(new Error(`download failed: code = ${res.code}`));
+				return;
+			}
+			resolve(body);
+		});
+	});
 }
 
 function _extractZip(buf: Buffer, dest: string): Promise<void> {
