501dbc88da38d651716bdc863dc30c285b30dda1
xnv <xnv@users.noreply.github.com>
2021-11-25 10:45:12 +0900

    fix(init): fix broken template.prompt.js evaluation

--- packages/akashic-cli-init/src/init/prompt.ts
+++ packages/akashic-cli-init/src/init/prompt.ts
@@ -1,26 +1,35 @@
-import { readdirRecursice, rename, rm, writeFile } from "@akashic/akashic-cli-commons/lib/FileSystem";
 import * as fs from "fs";
 import * as path from "path";
-import * as prompts from "prompts";
-import * as ejs from "ejs";
+import type { PromptObject } from "prompts";
+import { readdirRecursice, rename, rm, writeFile } from "@akashic/akashic-cli-commons/lib/FileSystem";
 
-export async function readPromptConfig(baseDir: string): Promise<prompts.PromptObject | prompts.PromptObject[] | null> {
+export async function readPromptConfig(baseDir: string): Promise<PromptObject | PromptObject[] | null> {
 	const promptConfigPath = path.join(baseDir, "template.prompt.js");
 	if (!fs.existsSync(promptConfigPath))
 		return null;
-	const { VM } = await import("vm2");
-	const vm = new VM({ timeout: 1000 });
-	return vm.runFile(promptConfigPath) as (prompts.PromptObject | prompts.PromptObject[]);
+	const { NodeVM } = await import("vm2");
+	const vm = new NodeVM();
+	return vm.runFile(promptConfigPath) as (PromptObject | PromptObject[]);
 }
 
 export async function replaceEJSWithRendered(baseDir: string, data: any): Promise<void> {
+	const ejs = await import("ejs");
 	const filepaths = await readdirRecursice(baseDir);
 	for (const filepath of filepaths) {
 		if (/\.ejs$/.test(filepath)) {
-			await writeFile(filepath.replace(/\.ejs$/, ""), await ejs.renderFile(filepath, data));
+			const dest = filepath.replace(/\.ejs$/, "");
+			const rendered = (await ejs.renderFile(filepath, data)) as string;
+			const cont = /\.json$/.test(dest) ? prettyPrintJson(rendered) : rendered;
+			await writeFile(dest, cont);
 			await rm(filepath);
 		} else if (/\.raw$/.test(filepath)) {
 			await rename(filepath, filepath.replace(/\.raw$/, ""));
 		}
 	}
+}
+
+function prettyPrintJson(str: string): string {
+	const m = str.match(/(?:\r|\n)(\s+)/);
+	const spacer = m ? m[1] : 2;
+	return JSON.stringify(JSON.parse(str), null, spacer);
 }
