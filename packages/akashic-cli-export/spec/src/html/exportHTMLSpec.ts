44f96e46fd957bc3a2ec18fcd4ff6db379fb91af
shinobu-takahashi <shinobu_takahashi@c.dwango.co.jp>
2022-01-25 18:20:46 +0900

    Add debugOverrideEngineFiles option test

--- packages/akashic-cli-export/spec/src/html/exportHTMLSpec.ts
+++ packages/akashic-cli-export/spec/src/html/exportHTMLSpec.ts
@@ -111,4 +111,61 @@ describe("exportHTML", function () {
 			.then(done, done.fail);
 	});
 
+	it("promiseExportHTML with debugOverrideEngineFiles option", (done) => {
+		Promise.resolve()
+			.then(() => {
+				const param: exp.ExportHTMLParameterObject = {
+					logger: undefined,
+					cwd: path.join(__dirname, "..", "..", "fixtures", "sample_game_v3"),
+					source: ".",
+					output: undefined,
+					force: true,
+					strip: false,
+					minify: false,
+					magnify: false,
+					unbundleText: false,
+					lint: false,
+					debugOverrideEngineFiles: path.join(__dirname, "..", "..", "fixtures", "engineFilesV3_1_99.js")
+				};
+				return exp.promiseExportHTML(param);
+			})
+			.then((dest) => {
+				expect(dest).toMatch(/^.*akashic-export-html-tmp-.+$/);
+				expect(fsx.statSync(path.join(dest, "js", "engineFilesV3_1_99.js"))).toBeTruthy();
+				const buff = fsx.readFileSync(path.join(dest, "index.html"));
+				// index.html で指定したengineFiles が読み込まれている
+				expect(buff.toString().includes("<script src=\"./js/engineFilesV3_1_99.js\"")).toBeTruthy();
+				fsx.removeSync(dest);
+			})
+			.then(done, done.fail);
+	});
+
+	it("promiseExportHTML with bundole and debugOverrideEngineFiles option", (done) => {
+		Promise.resolve()
+			.then(() => {
+				const param: exp.ExportHTMLParameterObject = {
+					logger: undefined,
+					cwd: path.join(__dirname, "..", "..", "fixtures", "sample_game_v3"),
+					source: ".",
+					output: undefined,
+					force: true,
+					strip: false,
+					minify: false,
+					magnify: false,
+					unbundleText: false,
+					lint: false,
+					bundle: true,
+					debugOverrideEngineFiles: path.join(__dirname, "..", "..", "fixtures", "engineFilesV3_1_99.js")
+				};
+				return exp.promiseExportHTML(param);
+			})
+			.then((dest) => {
+				expect(dest).toMatch(/^.*akashic-export-html-tmp-.+$/);
+				expect(fsx.existsSync(path.join(dest, "js"))).toBeFalsy();
+				const buff = fsx.readFileSync(path.join(dest, "index.html"));
+				expect(buff.toString().includes("dummy-engineFilesV3_1_99")).toBeTruthy(); // engineFiles の中身が index.html に存在する
+				fsx.removeSync(dest);
+			})
+			.then(done, done.fail);
+	});
 });
