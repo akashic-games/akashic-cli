2554173b7febbb5f71affb8ad127bce2163a8b2f
dera- <alchemy.and.fairy@gmail.com>
2022-03-03 09:28:58 +0000

    reflect pr comment

--- packages/akashic-cli-serve/src/client/akashic/GameViewManager.ts
+++ packages/akashic-cli-serve/src/client/akashic/GameViewManager.ts
@@ -4,11 +4,29 @@ import { NullScriptAssetV3 } from "./AssetV3";
 import { ServeGameContent } from "./ServeGameContent";
 import { generateTestbedScriptAsset } from "./TestbedScriptAsset";
 
+export interface RendererLike {
+	drawImage(
+		surface: SurfaceLike,
+		offsetX: number,
+		offsetY: number,
+		width: number,
+		height: number,
+		destOffsetX: number,
+		destOffsetY: number
+	): void;
+}
+
+export interface SurfaceLike {
+	width: number;
+	height: number;
+	renderer(): RendererLike;
+}
+
 export interface Platform {
-	getPrimarySurface: () => any; // 戻り値は `g.Surface` (コンパイル時に g がないので any で妥協)
+	getPrimarySurface: () => SurfaceLike;
 	_resourceFactory: {
 		createScriptAsset: (id: string, path: string) => any;  // 戻り値は `g.ScriptAsset` (コンパイル時に g がないので any で妥協)
-		createSurface: (width: number, height: number) => any; // 戻り値は `g.Surface` (コンパイル時に g がないので any で妥協)
+		createSurface: (width: number, height: number) => SurfaceLike;
 	};
 }
 
@@ -162,31 +180,47 @@ export class GameViewManager {
 				() => options.g.ExceptionFactory.createAssetLoadError("can not load script: " + assetPath)
 			);
 		};
-		// 描画元の範囲が指定されたwidth,heightを超える値もしくは0以下が与えられた場合Safariでのみ描画されないという問題が発生するので、g.Surface#renderer()でエラーを投げる処理を差し込む
-		// funcにはg.Surfaceを返す関数が渡されることを想定している。本来は型で縛るべきだがコンパイル時に g がないのでFunction型で妥協。
-		function createMeddlingWrappedSurfaceFactory(func: Function) {
+		// 一部のエッジケースでSafariのみ描画されないという問題が発生するので、ゲーム開発者が開発中に気づけるようにg.Renderer#drawImage()でエラーを投げる処理を差し込む
+		function createMeddlingWrappedSurfaceFactory <T extends ((...args: any[]) => SurfaceLike)> (
+			func: T
+		) {
 			return function() {
 				const surface = func.apply(this, arguments);
 				const originalRenderer = surface.renderer;
-				// drawImageメソッドの中で元のdrawImageメソッドを利用する実装のため、rendererをキャッシュしないとrenderer呼び出しの度にバリデーション処理が増えてしまう
-				// 本来なら前回のrendererの内容と比較してdiffがあるかを判定する対応にすべきだが、rendererの内容は不変なので単純にrendererをキャッシュするだけの対応としている
-				let rendererCache: any = null;
+				let renderer: RendererLike = null;
 				surface.renderer = function () {
-					if (rendererCache) {
-						return rendererCache;
+					// surface.renderer() はコンテンツから描画のたびに呼び出されるが戻り値は現実的に固定なので、ここでの surface.renderer() の上書きは一度しか適用しない
+					if (renderer) {
+						return renderer;
 					}
-					const renderer = originalRenderer.apply(this);
+					renderer = originalRenderer.apply(this);
 					const originalDrawImage = renderer.drawImage;
-					renderer.drawImage = function (surface: any, offsetX: number, offsetY: number, width: number, height: number) {
+					renderer.drawImage = function (
+						surface: SurfaceLike,
+						offsetX: number,
+						offsetY: number,
+						width: number,
+						height: number,
+						_destOffsetX: number,
+						_destOffsetY: number
+					) {
 						if (offsetX < 0 || offsetX + width > surface.width || offsetY < 0 || offsetY + height > surface.height) {
-							throw new Error(`Please draw with following range. x: 0-${surface.width}, y: 0-${surface.height}.`);
+							// ref. https://github.com/akashic-games/akashic-engine/issues/349
+							throw new Error(`drawImage(): out of bounds.`
+								+ `The source rectangle bleeds out the source surface (${surface.width}x${surface.height}). `
+								+ `This is not a bug but intentionally prohibited by akashic serve`
+								+ `to prevent platform-specific rendering trouble.`
+							);
 						}
 						if (width <= 0 || height <= 0) {
-							throw new Error(`Please set width and height to value higher than 0.`);
+							throw new Error(`drawImage(): nothing to draw.`
+								+ `Either width or height is less than or equal to zero.`
+								+ `This is not a bug but intentionally prohibited by akashic serve`
+								+ `to prevent platform-specific rendering trouble.`
+							);
 						}
 						originalDrawImage.apply(this, arguments);
 					}
-					rendererCache = renderer;
 					return renderer;
 				}
 				return surface;
