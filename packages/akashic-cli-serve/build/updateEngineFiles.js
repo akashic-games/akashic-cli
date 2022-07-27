7acde35284e75c5eebf033e957c7e078e367cc97
shinobu-takahashi <shinobu_takahashi@c.dwango.co.jp>
2022-07-27 19:12:38 +0900

    Fix updateEngineFiles.js

--- packages/akashic-cli-serve/build/updateEngineFiles.js
+++ packages/akashic-cli-serve/build/updateEngineFiles.js
@@ -1,55 +1,43 @@
 const path = require("path");
 const fs = require("fs");
 
-console.log("start to generate engineFilesVersion.json");
-const json = JSON.parse(fs.readFileSync(path.resolve(__dirname, "..", "package.json"), "utf8"));
-let v1Version = "";
-let v2Version = "";
-let v3Version = "";
-
-for (let key of Object.keys(json.devDependencies)) {
-	const value = json.devDependencies[key];
-	if (/^npm:@akashic\/engine\-files@1\.\d+\.\d+/.test(value)) {
-		v1Version = value.split("@")[2];
-	}
-	if (/^npm:@akashic\/engine\-files@2\.\d+\.\d+/.test(value)) {
-		v2Version = value.split("@")[2];
-	}
-	if (/^npm:@akashic\/engine\-files@3\.\d+\.\d+/.test(value)) {
-		v3Version = value.split("@")[2];
-	}
-};
-
 const versions = {
 	v1: {
-		version: v1Version,
-		fileName: `engineFilesV${v1Version.replace(/[\.-]/g, "_")}.js`
+		version: "",
+		fileName: ""
 	},
 	v2: {
-		version: v2Version,
-		fileName: `engineFilesV${v2Version.replace(/[\.-]/g, "_")}.js`
+		version: "",
+		fileName: ""
 	},
 	v3: {
-		version: v3Version,
-		fileName: `engineFilesV${v3Version.replace(/[\.-]/g, "_")}.js`
+		version: "",
+		fileName: ""
 	}
 };
-
-const versionFIlePath = path.resolve(__dirname, "..", "config", "engineFilesVersion.json");
-fs.writeFileSync(versionFIlePath, JSON.stringify(versions, null, 2));
-console.log("end to generate files")
 
 try {
 	console.log("start to copy engineFiles.js");
 	for (let key of Object.keys(versions)) {
-		const version = versions[key];
-		const dest = path.resolve(__dirname, "..", "www", "public", "external", version.fileName);
-		if (fs.existsSync(dest)) continue;
+		const entryPath = require.resolve(`ae${key}`);
+		const rootPath = path.dirname(entryPath); // index.js と package.json が同層にあることが前提
+		const version = require(path.join(rootPath, "package.json")).version;
+		const fileName = `engineFilesV${version.replace(/[\.-]/g, "_")}.js`;
+		const engineFilesPath = path.join(rootPath, `dist/raw/release/full/${fileName}`);
 
-		const engineFilePath = path.resolve(`./node_modules/ae${key}/dist/raw/release/full/${version.fileName}`);
-		fs.copyFileSync(engineFilePath, dest);
+		versions[key].version = version;
+		versions[key].fileName = fileName;
+
+		const destPath = path.resolve(__dirname, "..", "www", "public", "external", fileName);
+		if (fs.existsSync(destPath)) continue;
+		fs.copyFileSync(engineFilesPath, destPath);
 	}
 	console.log("end to copy engineFiles.js");
-} catch(e) {
+
+	console.log("start to generate engineFilesVersion.json");
+	const versionFIlePath = path.resolve(__dirname, "..", "config", "engineFilesVersion.json");
+	fs.writeFileSync(versionFIlePath, JSON.stringify(versions, null, 2));
+	console.log("end to generate files");
+} catch (e) {
 	console.error(e);
 }
