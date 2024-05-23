67a711d230e448a5c361633ec15e3ca6833d8f47
yu-ogi <yusuke_ogiwara@dwango.co.jp>
2024-05-23 12:06:51 +0900

    fix(sandbox): fix lint error

--- packages/akashic-cli-sandbox/src/client/global.d.ts
+++ packages/akashic-cli-sandbox/src/client/global.d.ts
@@ -1,8 +1,10 @@
+/* eslint-disable @typescript-eslint/consistent-type-imports */
+
 interface Window {
 	gScriptContainer: {[key: string]: Function};
 }
 
-declare var g: typeof import("@akashic/akashic-engine");
+declare let g: typeof import("@akashic/akashic-engine");
 type Trigger<T> = import("@akashic/akashic-engine").Trigger<T>;
 type Asset = import("@akashic/akashic-engine").Asset;
 type AssetLoadHandler = import("@akashic/akashic-engine").AssetLoadHandler;
