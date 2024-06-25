0ce60579470e99d8a057f4a834968fbffcef2da9
xnv <xnv@users.noreply.github.com>
2024-06-25 16:13:36 +0900

    fix(stat): fix tests of stat for binary and vector-image

--- packages/akashic-cli-extra/spec/src/stat/statSpec.ts
+++ packages/akashic-cli-extra/spec/src/stat/statSpec.ts
@@ -113,12 +113,14 @@ describe("stat.size with incomplete game.json", () => {
 describe("format stat result", () => {
 	const expectedText =
 		"image: 144B (2%)\n" +
+		"vector-image: 0B (0%)\n" +
 		"text: 0B (0%)\n" +
 		"ogg audio: 7.45KB (88%)\n" +
 		"mp4 audio: 5.31KB\n" +
 		"aac audio: 4.16KB\n" +
 		"m4a audio: 4.75KB\n" +
 		"script: 494B (6%)\n" +
+		"binary: 0B (0%)\n" +
 		"other: 367B (4%)\n" +
 		"  game.json: 367B\n" +
 		"[*] TOTAL SIZE (using ogg): 8.43KB (8632B)\n" +
@@ -148,10 +150,12 @@ describe("format stat result", () => {
 describe("format stat result - drop aac", () => {
 	const expectedText =
 		"image: 0B (0%)\n" +
+		"vector-image: 0B (0%)\n" +
 		"text: 0B (0%)\n" +
 		"ogg audio: 7.45KB (98%)\n" +
 		"m4a audio: 4.75KB\n" +
 		"script: 0B (0%)\n" +
+		"binary: 0B (0%)\n" +
 		"other: 169B (2%)\n" +
 		"  game.json: 169B\n" +
 		"[*] TOTAL SIZE (using ogg): 7.61KB (7796B)\n" +
@@ -178,11 +182,13 @@ describe("format stat result - drop aac", () => {
 describe("format stat result - maximum mp4, drop m4a", () => {
 	const expectedText =
 		"image: 0B (0%)\n" +
+		"vector-image: 0B (0%)\n" +
 		"text: 0B (0%)\n" +
 		"ogg audio: 7.45KB\n" +
 		"mp4 audio: 9.31KB (98%)\n" +
 		"aac audio: 4.16KB\n" +
 		"script: 0B (0%)\n" +
+		"binary: 0B (0%)\n" +
 		"other: 168B (2%)\n" +
 		"  game.json: 168B\n" +
 		"[ ] TOTAL SIZE (using ogg): 7.61KB (7795B)\n" +
@@ -212,10 +218,12 @@ describe("format stat result - drop m4a and aac", () => {
 	const expectedText =
 		"WARN: audio/dummy.m4a or .aac, No such file.\n" +
 		"image: 144B (2%)\n" +
+		"vector-image: 0B (0%)\n" +
 		"text: 0B (0%)\n" +
 		"ogg audio: 7.45KB (88%)\n" +
 		"mp4 audio: 5.31KB\n" +
 		"script: 494B (6%)\n" +
+		"binary: 0B (0%)\n" +
 		"other: 367B (4%)\n" +
 		"  game.json: 367B\n" +
 		"[*] TOTAL SIZE (using ogg): 8.43KB (8632B)\n" +
@@ -229,6 +237,38 @@ describe("format stat result - drop m4a and aac", () => {
 			buffer += msg + "\n";
 		} });
 		const basepath = path.join(__dirname, "..", "..", "fixtures", "dummyGame5");
+		return commons.ConfigurationFile.read(
+			path.join(basepath, "game.json"),
+			logger
+		)
+			.then(game => stat.size({ logger, basepath, game, raw: false }))
+			.then(() => {
+				expect(buffer).toBe(expectedText);
+			})
+			.then(done, done.fail);
+	});
+});
+
+
+describe("format stat result - vector-image and binary", () => {
+	const expectedText =
+		"image: 0B (0%)\n" +
+		"vector-image: 159B (16%)\n" +
+		"text: 0B (0%)\n" +
+		"ogg audio: 0B\n" +
+		"script: 494B (49%)\n" +
+		"binary: 9B (1%)\n" +
+		"other: 355B (35%)\n" +
+		"  game.json: 355B\n" +
+		"[ ] TOTAL SIZE (using ogg): 1017B (1017B)\n" +
+		"[*] TOTAL SIZE (using aac): 1017B (1017B)\n";
+
+	it("will output following text", done => {
+		let buffer = "";
+		const logger = new commons.ConsoleLogger({ debugLogMethod: msg => {
+			buffer += msg + "\n";
+		} });
+		const basepath = path.join(__dirname, "..", "..", "fixtures", "dummyGame6_bin_vector");
 		return commons.ConfigurationFile.read(
 			path.join(basepath, "game.json"),
 			logger
