aaf35391c74f08e1cd83b158f9a53e06060f7354
xnv <xnv@users.noreply.github.com>
2024-12-20 19:57:04 +0900

    feat(serve): reject invalid use of nicoliveComment

--- packages/akashic-cli-serve/src/client/akashic/plugin/NicoliveCommentClientPlugin.ts
+++ packages/akashic-cli-serve/src/client/akashic/plugin/NicoliveCommentClientPlugin.ts
@@ -1,4 +1,5 @@
 import { Trigger } from "@akashic/trigger";
+import { callOrThrow } from "../../../common/callOrThrow";
 import type { NicoliveCommentPlugin } from "../../../common/types/NicoliveCommentPlugin";
 
 export class NicoliveCommentClientPlugin implements agv.ExternalPlugin {
@@ -6,13 +7,23 @@ export class NicoliveCommentClientPlugin implements agv.ExternalPlugin {
 	readonly onStartStop: Trigger<boolean> = new Trigger();
 
 	onload(game: agv.GameLike, _dataBus: unknown, _gameContent: agv.GameContent): void {
+		let started = false;
+
 		const exposed: NicoliveCommentPlugin = {
 			start: (_opts, callback) => {
+				if (started) {
+					callOrThrow(callback, new Error("g.game.nicoliveComment.start(): already started"));
+					return;
+				}
+				started = true;
 				this.onStartStop.fire(true);
 				if (callback)
 					setTimeout(callback, 0);
 			},
 			stop: () => {
+				if (!started)
+					throw new Error("g.game.nicoliveComment.stop(): not started");
+				started = false;
 				this.onStartStop.fire(false);
 			},
 		};
--- packages/akashic-cli-serve/src/server/domain/nicoliveComment/NicoliveCommentPluginHost.ts
+++ packages/akashic-cli-serve/src/server/domain/nicoliveComment/NicoliveCommentPluginHost.ts
@@ -1,6 +1,7 @@
 import type { AMFlow } from "@akashic/amflow";
 import { TickIndex, type Tick } from "@akashic/playlog";
 import { Trigger } from "@akashic/trigger";
+import { callOrThrow } from "../../../common/callOrThrow";
 import { createNicoliveCommentMessageEvent } from "../../../common/PlaylogShim";
 import type { NicoliveCommentConfig, NicoliveCommentConfigComment } from "../../../common/types/NicoliveCommentConfig";
 import type { NicoliveComment, NicoliveCommentPlugin } from "../../../common/types/NicoliveCommentPlugin";
@@ -156,19 +157,3 @@ function filterProperty<T extends object>(o: T, filter: Set<keyof T>): Partial<T
 	});
 	return ret;
 };
-
-/**
- * callback が関数であるなら err を与えて呼び出す。でなければ err を throw する。
- *
- * 以下すべてを満たす時に利用すること。
- * (a) エラー通知用のコールバックが省略可能で、必ずしもそれでエラー通知ができない
- * (b) err がロジックエラー (コンテンツ開発者の対応が必要) であり、serve としてはゲーム開発者が確実に気づける形が望ましい
- * (c) 通知したいエラーが同期的に発生している
- */
-function callOrThrow(callback: ((err?: Error) => void) | null | undefined, err: Error): void {
-	if (callback) {
-		callback(err);
-	} else {
-		throw err;
-	}
-}
