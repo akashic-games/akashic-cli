d57cf7e457db0c6dae63ba84d39848dc03b174b7
yu-ogi <yusuke_ogiwara@dwango.co.jp>
2025-08-06 13:24:26 +0900

    feat(serve): support 'nicolive:multi' in standalone mode

--- packages/akashic-cli-serve/src/client/sandbox/operator/Operator.ts
+++ packages/akashic-cli-serve/src/client/sandbox/operator/Operator.ts
@@ -4,6 +4,7 @@ import type { DumpedPlaylog } from "../../../common/types/DumpedPlaylog";
 import type { GameViewManager } from "../../akashic/GameViewManager";
 import { ServeMemoryAmflowClient } from "../../akashic/ServeMemoryAMFlowClient";
 import type { ClientContentLocator } from "../../common/ClientContentLocator";
+import { createSessionParameter } from "../../common/createSessionParameter";
 import type { ScreenSize } from "../../common/types/ScreenSize";
 import type { ExecutionMode } from "../../store/ExecutionMode";
 import { PlayEntity } from "../../store/PlayEntity";
@@ -126,8 +127,15 @@ export class Operator {
 			this.store.profilerStore.pushProfilerValueResult("rendering", value.renderingTime);
 		});
 
-		if (params?.joinsSelf) {
-			playEntity.join(player.id, player.name);
+		if (params?.joinsSelf || this.store.targetService === "nicolive:multi") {
+			if (store.player) {
+				playEntity.join(store.player.id, store.player.name);
+			}
+			const { events, autoSendEventName } = playEntity.content.sandboxConfig;
+			if (!events || (autoSendEventName && Array.isArray(events[autoSendEventName]))) {
+				// autoSendEvent が存在しない場合のみデフォルトのセッションパラメータを送る
+				playEntity.amflow.enqueueEvent(createSessionParameter(this.store.targetService));
+			}
 		}
 
 		this.play.sendAutoStartEvent();
